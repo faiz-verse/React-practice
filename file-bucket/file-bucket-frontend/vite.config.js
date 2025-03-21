87a8c00702339c755c8fb34aa115cfbc19c5caab
faiz-verse <faizzzverseee@gmail.com>
2025-03-21 12:13:33 +0530

    edited vite.config.js for trying to enable dynamic routing in Render.com

--- file-bucket/file-bucket-frontend/vite.config.js
+++ file-bucket/file-bucket-frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5000,
+  },
+  preview: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
