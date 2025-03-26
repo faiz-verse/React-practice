5f3e706188d7d9b15e0bc0b65f330c89f728054f
faiz-verse <faizzzverseee@gmail.com>
2025-03-26 12:42:33 +0530

    changed the CORS origin to frontend

--- file-bucket/file-bucket-backend/server.js
+++ file-bucket/file-bucket-backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "*" })); // Allow all origins
+app.use(cors({ origin: ["https://file-bucket.onrender.com"] })); // Allow file bucket frontend origin
 app.use(express.json());
 
 // MongoDB Connection
